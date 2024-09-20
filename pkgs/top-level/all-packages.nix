bb619b968357f3ac84827213efc76df6a89d3e8a
Honnip <me@honnip.page>
2024-09-20 15:44:10 +0900

    gnomeExtensions: Handle GNOME 47 extensions
    Applied from pull request 343905.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35454,6 +35454,7 @@ with pkgs;
     gnome44Extensions
     gnome45Extensions
     gnome46Extensions
+    gnome47Extensions
   ;
 
   gnome-extensions-cli = python3Packages.callPackage ../desktops/gnome/misc/gnome-extensions-cli { };
